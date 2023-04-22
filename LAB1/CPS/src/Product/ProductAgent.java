fae2d4888977eb563da157cc632d1938bb6c7bfd
philips <feduardomorais@gmail.com>
2023-04-22 18:45:46 +0100

    behaviours?
    Co-authored-by: inesoliveira1 <inesoliveira1@users.noreply.github.com>
    Co-authored-by: Marcos-Silva8 <Marcos-Silva8@users.noreply.github.com>

--- LAB1/CPS/src/Product/ProductAgent.java
+++ LAB1/CPS/src/Product/ProductAgent.java
@@ -1,6 +1,9 @@
 package Product;
 
 import jade.core.Agent;
+import jade.core.behaviours.SequentialBehaviour;
+import jade.core.behaviours.SimpleBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
 import jade.domain.df;
 import jade.core.AID;
 import jade.core.Agent;
@@ -15,11 +18,16 @@ import java.util.Vector;
  *
  * @author Ricardo Silva Peres <ricardo.peres@uninova.pt>
  */
+
 public class ProductAgent extends Agent {    
     
     String id;
     ArrayList<String> executionPlan = new ArrayList<>();
     // TO DO: Add remaining attributes required for your implementation
+    
+    /*private int currentStep = 0; // the current step in the execution plan
+    private boolean isPaused = false; // whether the product is currently paused
+    private ArrayList<AID> skillAgents = new ArrayList<>(); // list of skill agents for this product*/
     
     @Override
     protected void setup() {
@@ -30,18 +38,54 @@ public class ProductAgent extends Agent {
 
         // TO DO: Add necessary behaviour/s for the product to control the flow
         // of its own production
+        
+        /*SequentialBehaviour productionBehaviour = new SequentialBehaviour();
+        for (String skill : executionPlan) {
+            productionBehaviour.addSubBehaviour(new CFPBehaviourSkill(this, "Skill: " + skill));
+        }*/
+
 
         // setup initiator for CFP
         ACLMessage msgCFP = new ACLMessage(ACLMessage.CFP);
+        /*msgCFP.setContent("Request skill agents");
+        msgCFP.setOntology("skill-ontology");
+        for(String skill: executionPlan){
+            msgCFP.addReceiver(new AID(skill, AID.ISLOCALNAME));
+        }
+        addBehaviour((new initiatorCFPAgent(this, msgCFP)));*/
         msgCFP.addReceiver(new AID("responder", false));
         this.addBehaviour(new initiatorCFPAgent(this, msgCFP));
 
+
         // setup initiator for REQUEST
         ACLMessage msgRE = new ACLMessage(ACLMessage.REQUEST);
+        /*msgRE.setContent("Star production");
+        msgRE.setOntology("production-ontology");
+        for(AID skillAgent:skillAgents){
+            msgRE.addReceiver(skillAgent);
+        }
+        addBehaviour(new initiatorREAgent(this, msgRE));*/
+
         msgRE.addReceiver(new AID("responder", false));
         this.addBehaviour(new initiatorCFPAgent(this, msgRE));
 
+
+        // setup sequential behaviour
+        SequentialBehaviour sb = new SequentialBehaviour();
+        /*sb.addSubBehaviour(productionBehaviour);
+        sb.addSubBehaviour(new OneShotBehaviour() {
+            public void action(){
+                System.out.println("Production Complete!");
+            }
+        });*/
+        
+        
+        //sb.addSubBehaviour(new RequestSkillBehaviour(this, "CFP Skill"));
+        //sb.addSubBehaviour(new RequestTransportBehaviour(this, "REQUEST Transport"));
+        //sb.addSubBehaviour(new ExecutePlanBehaviour(this, "REQUEST Skill"));
+        this.addBehaviour(sb);
     }
+
     private class initiatorCFPAgent extends ContractNetInitiator {
         public initiatorCFPAgent(Agent a, ACLMessage msg){
             super(a, msg);
@@ -77,6 +121,53 @@ public class ProductAgent extends Agent {
             System.out.println(myAgent.getLocalName() + ": INFORM message received");
         }
     }
+
+    /*private class simpleBeh extends SimpleBehaviour {
+
+        private boolean finished = false;
+        int step = 0;
+        String printOut;
+
+        public simpleBeh(Agent a, String prtOut) {
+            super(a);
+            this.printOut = prtOut;
+        }
+
+        @Override
+        public void action() {
+            System.out.println("SimpleBehaviour: SubBehaviour:" + printOut + " - step: " + ++step);
+        }
+
+        @Override
+        public boolean done() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }*/
+
+    private class CFPBehaviourSkill extends SimpleBehaviour {
+
+        private boolean finished = false;
+        int step = 0;
+        String printOut;
+
+        public CFPBehaviourSkill(Agent a, String prtOut) {
+            super(a);
+            this.printOut = prtOut;
+        }
+
+        @Override
+        public void action() {
+            //System.out.println("SimpleBehaviour: SubBehaviour:" + printOut + " - step: " + ++step);
+        }
+
+        @Override
+        public boolean done() {
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            finished = true;
+            return finished;
+        }
+    }
+
 
     @Override
     protected void takeDown() {
