a1c4127e909bb6aef3b1e0ab8ebaf0c6f6f91fc5
philips <feduardomorais@gmail.com>
2023-05-22 20:54:33 +0100

    main.py testes por causa de erro no internal serv

--- LAB2/main.py
+++ LAB2/main.py
@@ -5,34 +5,44 @@ import numpy as np
 import uvicorn
 import pickle
 import sklearn
+from sklearn import tree
 
 
 app = FastAPI()
-model1 = load("LAB2\joblibFiles\model1.joblib")
+#model1 = load(r"LAB2/joblibFiles/model1.joblib")
+#model2 = load(r"LAB2/joblibFiles/model2.joblib")
+#model3 = load(r"LAB2/joblibFiles/model3.joblib")
+#model4 = load(r"LAB2/joblibFiles/model4.joblib")
 
 class InputData(BaseModel):
     # Define the necessary fields for your input data
-    speed: str
+    speed: int
     action: int
-    # Add more fields as required
 
-#with open("LAB2\pickleFiles\model1_pickle.pickle", "rb") as f:
-#    model = pickle.load(f)
+model1 = pickle.load(open('model1_pickle.pkl', 'rb'))
 
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the API!"}
+
+@app.get("/favicon.ico")
+def get_favicon():
+    return ""
 
 @app.post("/predict")
 def predict(data: InputData):
     # Extract the required fields from the input data
-    speed = data.field1
-    action = data.field2
+    speed = data.speed
+    action = data.action
 
     # Perform any necessary preprocessing on the input data
+    data = [[speed, action]]
 
     # Make the prediction using the loaded model
-    prediction = model1.predict([[speed, action]])  # Adjust based on your model's input format
+    prediction = model1.predict([[50, 2]])  # Adjust based on your model's input format
 
     # Process and return the prediction result
     return {"prediction": prediction}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="10.22.113.42", port = 8000)
+    uvicorn.run(app, host="127.0.0.1", port = 8000)
